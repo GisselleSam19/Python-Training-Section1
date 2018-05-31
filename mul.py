31a2d3c31b5929ddf78daef1052661addb3f9ec2
GisselleSam19 <39727429+GisselleSam19@users.noreply.github.com>
2018-05-31 10:41:10 +0530

    Add files via upload

---
+++ mul.py
@@ -0,0 +1,7 @@
+def mul(n):
+	for i in range(1,11):
+		print n,"*", i ,"=" ,n*i
+
+num=input("Enter a Number : ")
+print "Multiplication Table of ",num
+mul(num)
---
+++ name.py
@@ -0,0 +1,5 @@
+name = raw_input("Enter a Name : ")
+l=len(name)
+for i in range(0,l,2):
+	print name[i]
+ 
